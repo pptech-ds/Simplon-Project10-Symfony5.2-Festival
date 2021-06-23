a9f6c39268f7e4e409c8f338af998c43606f40f3
pras <pptech.ds@gmail.com>
2021-06-24 01:10:56 +0200

    Artists page fixed by category filtering

--- src/Repository/ArtistRepository.php
+++ src/Repository/ArtistRepository.php
@@ -38,60 +38,6 @@ class ArtistRepository extends ServiceEntityRepository
     }
 
 
-    // public function findByCategory(int $categoryId): array
-    // {
-    //     $entityManager = $this->getEntityManager();
-
-    //     $query = $entityManager->createQuery(
-    //         'SELECT a.name, a.isLive, a.description
-    //         FROM App\Entity\Artist a
-    //         INNER JOIN App\Entity\Category c ON a.category = c.id
-    //         WHERE c.id =:categoryIdToSet'
-    //     )
-    //     ->setParameter('categoryIdToSet', $categoryId)
-    //     ;
-
-    //     dd($query->getResult());
-
-    //     return $query->getResult();
-    // }
-    
-    
-
-    // /**
-    //  * @return Artist[] Returns an array of Artist objects
-    //  */
-    
-    // public function findByCategory($value)
-    // {
-    //     return $this->createQueryBuilder('a')
-    //         ->select('a.name' , 'a.isLive', 'a.description')
-    //         ->innerJoin('c.id', 'a', 'WITH', 'a.category = c.id')
-    //         ->andWhere('c.id = :val')
-    //         ->setParameter('val', $value)
-    //         ->getQuery()
-    //         ->getResult()
-
-    //         // innerJoin('c.phones', 'p', Join::ON, 'c.id = p.customerId')
-
-
-    //         // ->andWhere('a.exampleField = :val')
-    //         // ->setParameter('val', $value)
-    //         // ->orderBy('a.id', 'ASC')
-    //         // ->setMaxResults(10)
-    //         // ->getQuery()
-    //         // ->getResult()
-    //     ;
-
-
-    //     // $qb->select('c')
-    //     // ->innerJoin('c.phones', 'p', 'WITH', 'p.phone = :phone')
-    //     // ->where('c.username = :username');
-
-        
-    // }
-    
-
 
     // /**
     //  * @return Artist[] Returns an array of Artist objects
