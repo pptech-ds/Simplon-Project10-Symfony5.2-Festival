e47284d28f93ab4620b0c888975676abb194ee26
pras <pptech.ds@gmail.com>
2021-06-24 02:55:29 +0200

    fixtures with fake data fixed

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $categories = [
+            1 => [
+                'name' => 'Mélodique',
+                'color' => 'primary',
+            ],
+            2 => [
+                'name' => 'Industrielle',
+                'color' => 'secondary',
+            ],
+            3 => [
+                'name' => 'Groovy',
+                'color' => 'success',
+            ],
+            4 => [
+                'name' => 'Deep',
+                'color' => 'info',
+            ],
+            5 => [
+                'name' => 'Détroit',
+                'color' => 'warning',
+            ],
+        ];
+
+        foreach ($categories as $key => $value) {
+            $category = new Category;
+            $category->setName($value['name']);
+            $category->setColor($value['color']);
+            $manager->persist($category);
+
+            // Enregistre la catégorie dans une référence
+            $this->addReference('category_' . $key, $category);
+        }
+        $manager->flush();
+    }
+}
