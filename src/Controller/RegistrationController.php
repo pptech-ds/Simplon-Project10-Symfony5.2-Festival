52e0f87f5cb0081490084e87a2a6de112d405500
pras <pptech.ds@gmail.com>
2021-06-27 15:04:09 +0200

    form management bug fix

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -60,7 +60,7 @@ class RegistrationController extends AbstractController
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
-            $this->addFlash('success', 'A confirmation email has been sent');
+            $this->addFlash('success', 'Un email de confirmation vous a été envoyé, merci de verifier votre email et de valider via le lien');
 
             // return $guardHandler->authenticateUserAndHandleSuccess(
             //     $user,
@@ -94,7 +94,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('verify_email_success', 'Votre email a bien été vérifié, bienvenue sur notre site !');
 
         return $this->redirectToRoute('app_login');
     }
