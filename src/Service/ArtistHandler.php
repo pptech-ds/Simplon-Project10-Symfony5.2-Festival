b01ee1bfe195f24d035c37fe1587d388340bc358
pras <pptech.ds@gmail.com>
2021-07-01 03:05:41 +0200

    billetterie form optimized

--- src/Service/ArtistHandler.php
+++ src/Service/ArtistHandler.php
@@ -4,7 +4,7 @@ namespace App\Service;
 
 class ArtistHandler
 {
-    public function handle($artists)
+    public function setColor($artists)
     {
         $categoryColors = [
             'Mélodique' => 'primary',
